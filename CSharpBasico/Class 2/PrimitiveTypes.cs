65d57adce23a1464b2336ab07f9011de748bec52
Edgar Santiago <itt.edgarstgo@gmail.com>
2020-06-26 10:13:15 -0400

    upd

---
+++ CSharpBasico/Class 2/PrimitiveTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasico.Class_2
+{
+    class PrimitiveTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
