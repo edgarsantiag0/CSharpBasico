3998420a32b4f5cd56d120dc8fafc572ce389064
Edgar Santiago <itt.edgarstgo@gmail.com>
2020-07-16 22:23:46 -0400

    Update clase 7

---
+++ CSharpBasico/Enums.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasico
+{
+   
+    public enum Size : int
+    {
+        Small, // 0
+        Normal, //1
+        Large  //2
+    }
+
+    enum Size2 : int
+    {
+        Small = 1,  // 1
+        Normal,     //2
+        Large = 5  //5
+    }
+
+    enum Size3 : int
+    {
+        Small = 5,  // 5
+        Normal,     //6
+        Large = 7  //7
+    }
+
+
+    public class PlayEnum
+    {
+
+        public void Metodo(Size size)
+        {
+            if(size == Size.Large)
+            {
+                Console.WriteLine("es Large");
+            }
+
+            if (size == Size.Normal)
+            {
+                Console.WriteLine("es Normal");
+            }
+
+        }
+
+    }
+
+
+}
+
+      
+
---
+++ CSharpBasico/SummaryText.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasico
+{
+    class SummaryText
+    {
+        static void Main(string[] args)
+        {
+            // POST = C# se actualiza a su nuerva versión
+
+            // PREVIEW: Este es un mensaje del preview del texto dentro...
+
+            var sentence = "Este es un mensaje del preview del texto dentro del post que habla sobre la nueva version de c#.";
+
+            Console.WriteLine(Summarize(sentence, 25));
+            Console.WriteLine(Summarize(sentence, 20));
+            Console.WriteLine(Summarize(sentence, 30));
+            Console.WriteLine(Summarize(sentence, 31));
+
+
+        }
+
+        public static string Summarize(string sentence, int maxLength)
+        {
+
+            if (sentence.Length < maxLength)
+                return sentence;
+            else
+            {
+
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+
+                    if (totalCharacters > maxLength)
+                        break;
+
+                }
+
+                var summary = string.Join(" ", summaryWords) + "...";
+
+                return summary;
+
+            }
+        }
+    }
+}
